02c34a24036e3521f83bed622927771926b11cd5
NotNite <hi@notnite.com>
2023-01-15 18:49:38 -0500

    Add barebones solution

---
+++ src/XIVLauncher2.Common/Class1.cs
@@ -0,0 +1,6 @@
+﻿namespace XIVLauncher2.Common;
+
+public class Class1
+{
+    // pretend i put something here, got it?
+}
---
+++ src/XIVLauncher2/MainWindow.axaml.cs
@@ -0,0 +1,11 @@
+using Avalonia.Controls;
+
+namespace XIVLauncher2;
+
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+}
