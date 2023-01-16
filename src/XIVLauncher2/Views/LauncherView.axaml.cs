477408fff697e917b82d018879de969a20f5d6fb
NotNite <hi@notnite.com>
2023-01-16 18:50:21 -0500

    'yea i think thats what we want' - blems

---
+++ src/XIVLauncher2/Views/LauncherView.axaml.cs
@@ -0,0 +1,18 @@
+﻿using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace XIVLauncher2.Views;
+
+public partial class LauncherView : UserControl
+{
+    public LauncherView()
+    {
+        InitializeComponent();
+    }
+
+    private void InitializeComponent()
+    {
+        AvaloniaXamlLoader.Load(this);
+    }
+}
